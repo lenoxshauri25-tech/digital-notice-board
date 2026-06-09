e24c818c4dec15b01d4610f22b99479e700fe157
lenoxshauri25-Tech <lenoxshauri25@gmail.com>
2026-06-09 15:53:59 +0300

    Fix: added vercel deployment routing configuration

--- server.js
+++ server.js
@@ -7,7 +7,9 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-const PORT = 3000;
+// Change this line to let the cloud provider pick the active port dynamically
+const PORT = process.env.PORT || 3000;
+
 
 // Allow Express to read JSON data sent from forms
 app.use(express.json());
